c6e007b0e0fe66f356277a85dd1a5acefef227af
MarianacDias <marianacdias96@gmail.com>
2020-07-30 09:36:46 +0100

    pass reset done

--- cos/urls.py
+++ cos/urls.py
@@ -17,12 +17,22 @@ from django.contrib import admin
 from django.urls import path, include
 from webapp import views
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="webapp/index.html"), name="index"),
     path('admin/', admin.site.urls),
     path("webapp/", include("webapp.urls")),
+    path("login/", views.user_login, name="user_login"),
     path("logout/", views.user_logout, name="logout"),
-    path('', include('django.contrib.auth.urls'))
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='webapp/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='webapp/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='webapp/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='webapp/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from webapp import views
-from django.contrib.auth import views as auth_views
 
 app_name = "webapp"
 
@@ -8,7 +7,6 @@ urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register"),
     path("scientist_register/", views.scietist_register, name="scientist_reg"),
     path("donator_register/", views.donator_register, name="donator_reg"),
-    path("login/", views.user_login, name="user_login"),
     path("create/", views.ProjectCreateView, name="project_create"),
     path("list/<int:pk>", views.ProjectDetailView.as_view(), name="detail"),
     path("list/", views.ProjectListView.as_view(), name="list"),
@@ -17,13 +15,5 @@ urlpatterns = [
     path("donator/list_all", views.DonatorList, name="donator_list"),
     path("donator/<int:pk>", views.DataGiveView, name="register_don_project"),
     path("profile/", views.profileScientist, name='profile'),
-    path('password-reset/', auth_views.PasswordResetView.as_view(
-        template_name='webapp/password_reset.html'), name='password_reset'),
-    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='webapp/password_reset_done.html'), name='password_reset_done'),    
-    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='webapp/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='webapp/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
